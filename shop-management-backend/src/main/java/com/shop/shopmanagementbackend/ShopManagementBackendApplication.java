0870462ea28a15a2338f2068c43fc09923f3b778
FahimFoysalEmon <fahim.foysal.emon@gmail.com>
2024-02-01 12:40:24 +0600

    first commit

---
+++ shop-management-backend/src/main/java/com/shop/shopmanagementbackend/ShopManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.shop.shopmanagementbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopManagementBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopManagementBackendApplication.class, args);
+    }
+
+}
---
+++ shop-management-backend/src/test/java/com/shop/shopmanagementbackend/ShopManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.shopmanagementbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopManagementBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
